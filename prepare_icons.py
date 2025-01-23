314410e9feaf4047232053df1bd7d1c66505fa0a
Arleycht <47849356+Arleycht@users.noreply.github.com>
2025-01-23 12:51:21 -0500

    Add icon image

---
+++ prepare_icons.py
@@ -0,0 +1,8 @@
+from PIL import Image
+
+base_image = Image.open("base_icon.png")
+
+sizes = [48, 32, 16]
+images = [base_image.resize((size, size)) for size in sizes]
+
+base_image.save("main.ico", format="ICO", append_images=images)
